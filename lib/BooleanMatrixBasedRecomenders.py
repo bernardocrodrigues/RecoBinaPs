92561459c470b0e642542351060bd69746cdb63d
Bernardo Rodrigues <bernardocrodrigues@live.com>
2023-04-04 21:38:36 -0300

    feat: add FcaBmf

--- lib/BooleanMatrixBasedRecomenders.py
+++ lib/BooleanMatrixBasedRecomenders.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-from lib.FormalConceptAnalysis import BinaryDataset
+from lib.FormalConceptAnalysis import BinaryDataset, GreConD, get_factor_matrices_from_concepts
+from surprise import AlgoBase, PredictionImpossible
 from scipy.spatial import distance
 
 
@@ -40,3 +41,89 @@ def get_similarity_matrix(dataset: BinaryDataset, distance_strategy=jaccard_dist
     return similarity_matrix
 
 
+class FcaBmf(AlgoBase):
+    def __init__(self, k=30, coverage=1.0, distance_strategy=jaccard_distance, verbose=False):
+        AlgoBase.__init__(self)
+        self.verbose = verbose
+        self.k = k
+        self.coverage = coverage
+        self.distance_strategy = distance_strategy
+
+        self.actual_coverage = None
+        self.number_of_factors = None
+
+    def fit(self, trainset):
+        AlgoBase.fit(self, trainset)
+
+        if self.verbose:
+            print("[FcaBmf] Generating binary dataset...")
+
+        self.binary_dataset = BinaryDataset.load_from_trainset(trainset)
+
+        if self.verbose:
+            print("[FcaBmf] Generating binary dataset OK!")
+            print(f"[FcaBmf] Resulting binary dataset is {self.binary_dataset.shape[0]} rows x {self.binary_dataset.shape[1]} columns")
+
+        if self.verbose:
+            print("[FcaBmf] Generating Formal Context...")
+
+        self.formal_context, self.actual_coverage = GreConD(self.binary_dataset, coverage=self.coverage, verbose=self.verbose)
+        self.number_of_factors = len(self.formal_context)
+
+        if self.verbose:
+            print("[FcaBmf] Generating Formal Context OK")
+
+        self.Af, self.Bf = get_factor_matrices_from_concepts(self.formal_context, self.binary_dataset.shape[0], self.binary_dataset.shape[1])
+        latent_binary_dataset = BinaryDataset(self.Af)
+
+        if self.verbose:
+            print("[FcaBmf] Generating Similarity Matrix...")
+
+        self.sim = get_similarity_matrix(latent_binary_dataset, self.distance_strategy)
+
+        if self.verbose:
+            print("[FcaBmf] Generating Similarity Matrix Ok")
+
+        return self
+
+    def estimate(self, u, i):
+
+        if not (self.trainset.knows_user(u) and self.trainset.knows_item(i)):
+            raise PredictionImpossible("User and/or item is unknown.")
+
+        ruid = self.trainset.to_raw_uid(u)
+        riid = self.trainset.to_raw_iid(u)
+
+        rating = 0
+        weight_sum = 0
+        neighbors_used = []
+
+        neighbors = [(x2, self.sim[u, x2], r) for (x2, r) in self.trainset.ir[i]]
+        nearest_neighbors = sorted(neighbors, key=lambda d: d[1], reverse=True)
+
+        for neighbor in nearest_neighbors:
+
+            if len(neighbors_used) >= self.k:
+                break
+
+            neighbor_iid = neighbor[0]
+            neighbor_similarity = neighbor[1]
+            neighbor_rating = neighbor[2]
+
+            neighbor_ruid = self.trainset.to_raw_uid(neighbor_iid)
+
+            if neighbor_similarity == 0:
+                continue
+
+            rating += neighbor_similarity * neighbor_rating
+            weight_sum += neighbor_similarity
+            neighbors_used.append((neighbor_ruid, neighbor_similarity, neighbor_rating))
+
+        if not neighbors_used:
+            raise PredictionImpossible("Not enough neighbors.")
+
+        rating /= weight_sum
+
+        details = {"actual_k": len(neighbors_used), "neighbors_used": neighbors_used}
+
+        return rating, details
