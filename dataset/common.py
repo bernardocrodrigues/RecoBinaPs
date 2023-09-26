a840725b35ad8dc43800800ebbdd9f0c945d6bf0
Bernardo Rodrigues <bernardocrodrigues@live.com>
2023-09-26 07:25:01 -0300

    feat(dataset/common): add random dataset generation

--- dataset/common.py
+++ dataset/common.py
@@ -3,7 +3,10 @@
 This module contains common functions for the dataset module.
 
 """
+
+from typing import List, Tuple
 import numpy as np
+import numba as nb
 from surprise import Trainset
 
 
@@ -25,3 +28,60 @@ def convert_trainset_into_rating_matrix(trainset: Trainset) -> np.ndarray:
         rating_matrix[user_id, item_id] = rating
 
     return rating_matrix
+
+
+@nb.njit
+def generate_random_dataset(
+    number_of_users: int, number_of_items: int, rating_scale: int, sparsity_target: float
+) -> np.ndarray:
+    """
+    Generates a random dataset.
+
+    Args:
+        number_of_users (int): The number of users (rows).
+        number_of_items (int): The number of items (columns).
+        rating_scale (int): The rating scale. The ratings will be in the range [0, rating_scale].
+        sparsity_target (float): The sparsity target. The probability of any rating being generated
+            is equal to sparsity_target. Therefore, the sparsity of the dataset will be close to
+            sparsity_target.
+
+    Returns:
+        np.ndarray: The generated dataset.
+    """
+    dataset = np.zeros((number_of_users, number_of_items), dtype=np.float32)
+
+    for i in range(number_of_users):
+        for j in range(number_of_items):
+            if np.random.uniform() < sparsity_target:
+                dataset[i][j] = np.random.uniform() * rating_scale
+
+    return dataset
+
+
+@nb.njit
+def convert_to_raw_ratings(dataset: np.ndarray) -> List[Tuple[int, int, float, None]]:
+    """
+    Converts a dataset into a list of raw ratings as expected by surprise's Trainset class.
+
+    Args:
+        dataset (np.ndarray): The dataset to be converted.
+
+    Returns:
+        List[Tuple[int, int, float, None]]: The list of raw ratings.
+    """
+    raw_ratings = []
+
+    for i in range(dataset.shape[0]):
+        for j in range(dataset.shape[1]):
+            if dataset[i][j] != 0:
+                raw_ratings.append((i, j, float(dataset[i][j]), None))
+
+    return raw_ratings
+
+
+# Numba Compilation
+# Numba uses a Just-In-Time compiler to speed up the execution of the code. The functions need to
+# be ran once to be compiled. Therefore, we run the functions at import time to avoid the overhead
+# of compiling the functions when they are called.
+aux = generate_random_dataset(1, 1, 1, 1)
+aux_2 = convert_to_raw_ratings(aux)
