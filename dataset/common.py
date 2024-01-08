ae38529cb7bd4e6a267ed47a61badc1aa7e4279f
Bernardo Rodrigues <bernardocrodrigues@live.com>
2024-01-08 07:30:42 -0300

    refactor(dataset): remove unused convert_trainset_into_rating_matrix

--- dataset/common.py
+++ dataset/common.py
@@ -10,26 +10,6 @@ import numba as nb
 from surprise import Trainset
 
 
-def convert_trainset_into_rating_matrix(trainset: Trainset) -> np.ndarray:
-    """
-    Convert a surprise Trainset object into a rating matrix.
-
-    Args:
-        trainset (Trainset): The Trainset object.
-
-    Returns:
-        np.ndarray: The rating matrix.
-    """
-
-    shape = (trainset.n_users, trainset.n_items)
-
-    rating_matrix = np.zeros(shape)
-    for user_id, item_id, rating in trainset.all_ratings():
-        rating_matrix[user_id, item_id] = rating
-
-    return rating_matrix
-
-
 def load_dataset_from_trainset(trainset: Trainset) -> np.ndarray:
     """
     Loads a numpy array from a surprise Trainset object.
