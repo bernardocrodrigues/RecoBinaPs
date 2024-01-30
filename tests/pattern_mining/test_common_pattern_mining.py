1f99aee6ed086f0d817a843fcd94365fd60e91e1
Bernardo Rodrigues <bernardocrodrigues@live.com>
2024-01-30 16:29:12 -0300

    test(pattern_mining): add tests for applying bicluster coverage filter

--- tests/pattern_mining/test_common_pattern_mining.py
+++ tests/pattern_mining/test_common_pattern_mining.py
@@ -139,3 +139,52 @@ class TestApplyBiclusterSparsityFilter:
         assert len(filtered_patterns) == 1
         filtered_patterns = apply_bicluster_sparsity_filter(self.rating_dataset, patterns, 0.7)
         assert len(filtered_patterns) == 0
+
+
+class TestApplyBiclusterCoverageFilter:
+
+    rating_dataset = np.array(
+        [
+            [1.2, 2.3, 0.0, 0.0, 0.0],  # User 1
+            [0.0, 0.0, 2.3, 0.0, 0.0],  # User 2
+            [0.0, 0.0, 1.1, 0.0, 5.0],  # User 3
+            [0.0, 0.0, 0.0, 0.0, 0.0],  # User 4
+            [0.0, 0.0, 1.0, 0.0, 0.0],  # User 5
+        ],
+        dtype=np.float64,
+    )
+
+    def test_1(self):
+        patterns = [
+            create_concept(
+                np.array([0, 1, 2, 3, 4], dtype=np.int64), np.array([0, 1], dtype=np.int64)
+            ),
+            create_concept(
+                np.array([0, 1, 2, 3, 4], dtype=np.int64), np.array([0], dtype=np.int64)
+            ),
+            create_concept(
+                np.array([0, 1, 2, 3, 4], dtype=np.int64), np.array([4], dtype=np.int64)
+            ),
+            create_concept(
+                np.array([0, 1, 2, 3, 4], dtype=np.int64), np.array([2], dtype=np.int64)
+            ),
+            create_concept(
+                np.array([2], dtype=np.int64), np.array([0, 1, 2, 3, 4], dtype=np.int64)
+            ),
+            create_concept(
+                np.array([0, 2], dtype=np.int64), np.array([0, 1, 2], dtype=np.int64)
+            ),
+        ]
+
+        filtered_patterns = apply_bicluster_coverage_filter(self.rating_dataset, patterns, 0.6)
+        assert len(filtered_patterns) == 0
+
+        filtered_patterns = apply_bicluster_coverage_filter(self.rating_dataset, patterns, 0.5)
+        assert len(filtered_patterns) == 2
+
+        filtered_patterns = apply_bicluster_coverage_filter(self.rating_dataset, patterns, 0.2)
+        assert len(filtered_patterns) == 4
+
+        filtered_patterns = apply_bicluster_coverage_filter(self.rating_dataset, patterns, 0.1)
+        assert len(filtered_patterns) == 6
+
