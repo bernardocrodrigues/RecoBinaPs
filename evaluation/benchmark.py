fd7f58376cba1ad1c8877c6ef90beffe248fd556
Bernardo Rodrigues <bernardocrodrigues@live.com>
2024-05-24 09:47:01 -0300

    feat(evaluation): add cross_validate

--- evaluation/benchmark.py
+++ evaluation/benchmark.py
@@ -2,10 +2,33 @@ import time
 
 from typing import List, Tuple
 
+import numpy as np
 from surprise import AlgoBase, Trainset
 from pydantic import validate_call, ConfigDict
 
 from evaluation.strategies import TestMeasureStrategy, TrainMeasureStrategy
+
+
+def print_progress(tasks: List) -> None:
+    """
+    Print progress of a list of tasks.
+    """
+
+    total = len(tasks)
+    start_time = time.time()
+
+    # enumeration starts at 1 to avoid division by zero
+    for i, _ in enumerate(as_completed(tasks), 1):
+
+        now = time.time()
+        elapsed_time = now - start_time
+
+        average_time = elapsed_time / i
+        estimated_time_left = average_time * (total - i)
+
+        print(
+            f"Completed {i}/{total} | Avg. time/task: {average_time/60:.2f} min | Est. time left: {estimated_time_left/60:.2f} min"
+        )
 
 
 @validate_call(config=ConfigDict(strict=True, arbitrary_types_allowed=True, validate_return=True))
@@ -19,7 +42,7 @@ def fit_and_score(
     """
     Fit a recommender system on a trainset and evaluate it on a testset.
 
-    This function is based on Surprise's fit_and_score 
+    This function is based on Surprise's fit_and_score
     (surprise.model_selection.validation.fit_and_score) however it is somewhat
     simplified and adapted to:
     - work with our custom measures (modeled as strategies)
@@ -64,3 +87,62 @@ def fit_and_score(
     return test_measurements, train_measurements, fit_time, test_time
 
 
+@validate_call(config=ConfigDict(strict=True, arbitrary_types_allowed=True, validate_return=True))
+def cross_validate(
+    recommender_system: AlgoBase,
+    folds: List[Tuple[Trainset, List[Tuple[str, str, float]]]],
+    test_measures: List[TestMeasureStrategy] = [],
+    train_measures: List[TrainMeasureStrategy] = [],
+    max_workers=1,
+    verbose=True,
+):
+    """
+    Cross validate a recommender system.
+
+    This function is based on Surprise's cross_validate
+    (surprise.model_selection.validation.cross_validate) however it is somewhat
+    simplified and adapted to:
+    - work with our custom measures (modeled as strategies)
+    - process precomputed trainset and testset
+    - uses futures instead of parallel for parallelization
+    """
+
+    with ProcessPoolExecutor(max_workers=max_workers) as executor:
+
+        futures = [
+            executor.submit(
+                fit_and_score,
+                recommender_system,
+                trainset,
+                testset,
+                test_measures,
+                train_measures,
+            )
+            for trainset, testset in folds
+        ]
+
+        if verbose:
+            print_progress(futures)
+
+        out = [future.result() for future in futures]
+
+    (test_measures_dicts, train_measures_dicts, fit_times, test_times) = zip(*out)
+
+    measurements = {}
+
+    for train_measure in train_measures:
+        train_measure_name = train_measure.get_name()
+        measurements[train_measure_name] = np.asarray(
+            [d[train_measure_name] for d in train_measures_dicts]
+        )
+
+    for test_measure in test_measures:
+        test_measure_name = test_measure.get_name()
+        measurements[test_measure_name] = np.asarray(
+            [d[test_measure_name] for d in test_measures_dicts]
+        )
+
+    measurements["fit_time"] = list(fit_times)
+    measurements["test_time"] = list(test_times)
+
+    return measurements
