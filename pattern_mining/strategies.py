28d78b50d87d9d23a828d418aa2d950021b60d1c
Bernardo Rodrigues <bernardocrodrigues@live.com>
2024-06-28 17:53:37 -0300

    feat(pattern_mining/strategies): add QUBIC2Strategy

--- pattern_mining/strategies.py
+++ pattern_mining/strategies.py
@@ -11,16 +11,27 @@ See COPYING file for license details
 from abc import ABC, abstractmethod
 from typing import Annotated, List, Optional
 from tempfile import TemporaryDirectory
-from annotated_types import Gt, Le
+from annotated_types import Gt, Ge, Le
+from pathlib import Path
 
 from surprise import Trainset
 from pydantic import BaseModel, validate_call, ConfigDict, Field
 
 from pattern_mining.binaps.binaps_wrapper import run_binaps, get_patterns_from_weights
 from pattern_mining.formal_concept_analysis import create_concept
+from pattern_mining.qubic2 import (
+    fetch_qubic2_source_code,
+    compile_qubic2,
+    is_qubic2_available,
+    run_qubic2,
+    parse_biclusters_from_qubic_output,
+    QUBIC2_DESTINATION_PATH,
+)
 from dataset.binary_dataset import (
     load_binary_dataset_from_trainset,
+    load_discrete_dataset_from_trainset,
     save_as_binaps_compatible_input,
+    save_as_qubic2_compatible_input,
 )
 
 from .formal_concept_analysis import grecond, Concept
@@ -87,7 +98,7 @@ class GreConDStrategy(PatternMiningStrategy, BaseModel):
         self.actual_coverage = actual_coverage
 
         return formal_context
-    
+
     def __call__(self, trainset: Trainset) -> List[Concept]:
         return self.mine_patterns(trainset)
 
@@ -140,5 +151,55 @@ class BinaPsStrategy(PatternMiningStrategy, BaseModel):
         for pattern in patterns:
             concept = create_concept([], pattern)
             biclusters.append(concept)
+
+        return biclusters
+
+
+class QUBIC2Strategy(PatternMiningStrategy, BaseModel):
+    """
+    QUBIC2 strategy for pattern mining.
+
+    This strategy uses the QUBIC2 algorithm to mine the biclusters from the dataset. Before QUBIC2 is run,
+    the dataset is discretized.
+
+    """
+
+    bicluster_number: Annotated[int, Gt(0)] = 100
+    max_overlap: Annotated[float, Gt(0), Le(1)] = 1.0
+    consistency: Annotated[float, Gt(0.5), Le(1.0)] = 1.0
+    use_spearman_correlation: bool = False
+    minimum_column_width: Optional[Annotated[int, Ge(2)]] = None
+
+    @validate_call(
+        config=ConfigDict(strict=True, arbitrary_types_allowed=True, validate_return=True)
+    )
+    def mine_patterns(self, trainset: Trainset) -> List[Concept]:
+
+        if is_qubic2_available(QUBIC2_DESTINATION_PATH):
+            print("QUBIC2 is available")
+        else:
+            fetch_qubic2_source_code(QUBIC2_DESTINATION_PATH)
+            compile_qubic2(QUBIC2_DESTINATION_PATH)
+
+        dataset = load_discrete_dataset_from_trainset(trainset)
+
+        with TemporaryDirectory() as temporary_directory:
+
+            input_file_path = f"{temporary_directory}/dataset"
+            output_file_path = f"{temporary_directory}/dataset.blocks"
+
+            with open(input_file_path, "w+", encoding="UTF-8") as file_object:
+                save_as_qubic2_compatible_input(dataset, file_object)
+
+            run_qubic2(
+                data_path=Path(input_file_path),
+                bicluster_number=self.bicluster_number,
+                max_overlap=self.max_overlap,
+                consistency=self.consistency,
+                use_spearman_correlation=self.use_spearman_correlation,
+                minimum_column_width=self.minimum_column_width,
+            )
+
+            biclusters = parse_biclusters_from_qubic_output(output_file_path)
 
         return biclusters
