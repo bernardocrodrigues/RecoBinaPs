4d4fd3111693eef509351fdb5f7366bf9abc9fa5
Bernardo Rodrigues <bernardocrodrigues@live.com>
2024-08-01 19:56:41 -0300

    refactor(recommenders): remove broken automatic compilation of numba functions

--- recommenders/__init__.py
+++ recommenders/__init__.py
@@ -1,56 +1,5 @@
 import logging
 
-import numpy as np
-
-from .common import (
-    cosine_similarity,
-    adjusted_cosine_similarity,
-    user_pattern_similarity,
-    weight_frequency,
-    get_similarity,
-    get_similarity_matrix,
-    get_top_k_biclusters_for_user,
-    get_indices_above_threshold,
-    merge_biclusters,
-)
-
-from pattern_mining.formal_concept_analysis import create_concept
-
-
-def compile_numba():
-    DEFAULT_LOGGER.info("Forcing Numba compilation...")
-
-    u_float = np.array([1, 2, 3, 4, 5], dtype=np.float64)
-    v_float = np.array([1, 2, 3, 4, 5], dtype=np.float64)
-    u_int = np.array([1, 2, 3, 4, 5], dtype=np.int64)
-
-    concept = create_concept([1, 2, 3], [1, 2, 3])
-
-    dataset_float = np.array([[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]], dtype=np.float64)
-    dataset_int = np.array([[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]], dtype=np.int64)
-
-    cosine_similarity(u_float, v_float, 1e-7)
-    cosine_similarity(dataset_float[0], dataset_float[1])
-
-    adjusted_cosine_similarity(u_float, v_float)
-    adjusted_cosine_similarity(dataset_float[0], dataset_float[1])
-
-    user_pattern_similarity(u_int, concept)
-    user_pattern_similarity(dataset_int[0], concept)
-    
-    weight_frequency(dataset_int[0], concept)
-
-    get_similarity(0, 0, dataset_float)
-
-    get_similarity_matrix(dataset_float)
-
-    get_top_k_biclusters_for_user([concept], u_int, 1)
-
-    get_indices_above_threshold(u_int, 2)
-
-    merge_biclusters([concept, concept])
-
-    DEFAULT_LOGGER.info("Forcing Numba compilation OK")
 
 def build_logger(name, level=logging.INFO):
     logger = logging.getLogger(name)
@@ -68,5 +17,3 @@ def build_logger(name, level=logging.INFO):
 
 DEFAULT_LOGGER = build_logger("recommenders", logging.INFO)
 DEBUG_LOGGER = build_logger("recommenders debug", logging.DEBUG)
-
-compile_numba()
